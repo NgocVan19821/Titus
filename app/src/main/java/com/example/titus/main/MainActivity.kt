4f944d4b1b6a0f9e39127131b8abc140b7de5b3b
NgocVan19821 <ngocvan19821@gmail.com>
2023-02-12 17:19:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/titus/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.titus.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.titus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
